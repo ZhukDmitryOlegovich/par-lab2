ebd2e80e8edf93b5e3f25aa44eb6477b5590b09a
ZhukDima <zhuk@gmail.com>
2021-10-23 00:27:53 +0300

    Scripted auto-commit on change (2021-10-23 00:27:53) by gitwatch.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 mvn package
-hadoop fs -rm -r output
+hadoop fs -rm -f -r output
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
 hadoop App L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output
 rm -rf output
