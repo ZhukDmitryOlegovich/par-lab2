9ed35e466bfffe8203816ed2cd429bd2d8281672
ZhukDima <zhuk@gmail.com>
2021-10-23 01:45:59 +0300

    Scripted auto-commit on change (2021-10-23 01:45:59) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -1,10 +1,9 @@
-import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
     @Override
-    public int compare(@NonNull WritableComparable a, @NonNull WritableComparable b) {
+    public int compare(WritableComparable a, WritableComparable b) {
         return 0;
 //        return ((AirportJoinKey) a).getAirportId() - ((AirportJoinKey) b).getAirportId();
     }
--- src/main/java/PartitionerAirportJoinKey.java
+++ src/main/java/PartitionerAirportJoinKey.java
@@ -1,10 +1,9 @@
-import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.Text;
 
 public class PartitionerAirportJoinKey extends Partitioner<AirportJoinKey, Text> {
     @Override
-    public int getPartition(@NonNull AirportJoinKey airportJoinKey, Text text, int i) {
+    public int getPartition(AirportJoinKey airportJoinKey, Text text, int i) {
         return airportJoinKey.getAirportId() % i;
     }
 }
