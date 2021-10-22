dc13ccc026395afccebbe19bf174491d7323605c
ZhukDima <zhuk@gmail.com>
2021-10-22 22:23:52 +0300

    Scripted auto-commit on change (Fri Oct 22 22:23:51 MSK 2021)

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCountApp {
+public class AirportApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
             System.err.println("Usage: WordCountApp <input path> <output path>");
