5a9496e2c727b32b3cc809bccfd1f9fb1c72d578
ZhukDima <zhuk@gmail.com>
2021-10-23 00:15:46 +0300

    Scripted auto-commit on change (2021-10-23 00:15:46) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,8 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class App {
@@ -16,15 +16,23 @@ public class App {
         job.setJarByClass(App.class);
         job.setJobName("Airport-flight join");
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        {
+            MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
+            MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
 
-        job.setMapperClass(AirportMapper.class);
-        job.setReducerClass(WordReducer.class);
-        
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        job.setNumReduceTasks(2);
+            job.setGroupingComparatorClass(AirportComparator.class);
+            job.setReducerClass(DelayReducer.class);
+            job.setPartitionerClass(PartitionerAirportJoinKey.class);
+        }
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.setMapOutputKeyClass(AirportJoinKey.class);
+        job.setMapOutputValueClass(Text.class);
+
+//        job.setOutputKeyClass(Text.class);
+//        job.setOutputValueClass(IntWritable.class);
+//        job.setNumReduceTasks(2);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
