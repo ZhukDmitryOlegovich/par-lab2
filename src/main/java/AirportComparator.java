59f78847d2fb2e945a7a93567fbdebd0f1b37731
ZhukDima <zhuk@gmail.com>
2021-10-23 02:29:05 +0300

    Scripted auto-commit on change (2021-10-23 02:29:05) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -2,7 +2,9 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
-    AirportComparator() {}
+    public AirportComparator() {
+        super(AirportJoinKey.class, true);
+    }
 
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
