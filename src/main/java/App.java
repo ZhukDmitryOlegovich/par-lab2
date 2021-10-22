aa3c772cf8ae5a1518e738bb2ad4e7db5b3c42bd
ZhukDima <zhuk@gmail.com>
2021-10-23 00:09:19 +0300

    Scripted auto-commit on change (2021-10-23 00:09:19) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class AirportApp {
+public class App {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
             System.err.println("Usage: WordCountApp <input path> <output path>");
@@ -13,7 +13,7 @@ public class AirportApp {
         }
 
         Job job = Job.getInstance();
-        job.setJarByClass(AirportApp.class);
+        job.setJarByClass(App.class);
         job.setJobName("Word count");
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
