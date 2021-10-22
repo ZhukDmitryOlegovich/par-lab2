abe919e55ecb9373adf60dff5ab433917e5b41bf
ZhukDima <zhuk@gmail.com>
2021-10-22 23:44:14 +0300

    Scripted auto-commit on change (2021-10-22 23:44:14) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper<LongWritable, Text, AirportJoinKey, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportJoinKey, Text>.Context context)
+            throws IOException, InterruptedException {
+        if (key.get() == 0) {
+            return;
+        }
+
+        Airport airport = Airport.parseCSV(value.toString());
+        context.write(
+                new AirportJoinKey(airport.getCode(), true),
+                new Text(airport.getName())
+        );
+    }
 }
