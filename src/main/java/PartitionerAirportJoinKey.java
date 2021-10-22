232e1a3a4fd55f5edca2f4d1f4a837bef9a3ad71
ZhukDima <zhuk@gmail.com>
2021-10-22 23:50:52 +0300

    Scripted auto-commit on change (2021-10-22 23:50:52) by gitwatch.sh

--- src/main/java/PartitionerAirportJoinKey.java
+++ src/main/java/PartitionerAirportJoinKey.java
@@ -1,9 +1,9 @@
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.Text;
 
-public class Partitioner1 extends Partitioner<AirportJoinKey, Text> {
+public class PartitionerAirportJoinKey extends Partitioner<AirportJoinKey, Text> {
     @Override
     public int getPartition(AirportJoinKey airportJoinKey, Text text, int i) {
-        return airportJoinKey.getAirportId();
+        return airportJoinKey.getAirportId() % i;
     }
 }
