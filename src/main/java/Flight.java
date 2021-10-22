83396d1b8b7a14c27c82ad9a96a546c249484037
ZhukDima <zhuk@gmail.com>
2021-10-22 23:16:30 +0300

    Scripted auto-commit on change (2021-10-22 23:16:30) by gitwatch.sh

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,14 @@
 public class Flight {
+    private final float delay;
+    private final int code;
+
+    public Flight(float delay, int code) {
+        this.delay = delay;
+        this.code = code;
+    }
+
+    public static Flight parseCSV(String csv) {
+        String[] list = csv.replaceAll("\"", "").split(",");
+        return new Airport(Integer.parseInt(list[CODE_CSV_INDEX]), list[NAME_CSV_INDEX]);
+    }
 }
