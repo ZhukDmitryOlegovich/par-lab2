0660319ac5f4a1721faa4c7d36720749f50b612d
ZhukDima <zhuk@gmail.com>
2021-10-22 23:57:57 +0300

    Scripted auto-commit on change (2021-10-22 23:57:57) by gitwatch.sh

--- src/main/java/DelayReducer.java
+++ src/main/java/DelayReducer.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class DelayReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class DelayReducer extends Reducer<AirportJoinKey, Text, Text, Text> {
+    @Override
+    protected void reduce(AirportJoinKey key, Iterable<Text> values, Reducer<AirportJoinKey, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+        Iterator<Text> it = values.iterator();
+    }
 }
