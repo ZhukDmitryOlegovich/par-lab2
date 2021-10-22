cf7e9b374bc0b911bc79212e3f1c43cdcbd94335
ZhukDima <zhuk@gmail.com>
2021-10-23 01:45:17 +0300

    Scripted auto-commit on change (2021-10-23 01:45:17) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -1,9 +1,10 @@
+import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
     @Override
-    public int compare(WritableComparable a, WritableComparable b) {
+    public int compare(@NonNull WritableComparable a, @NonNull WritableComparable b) {
         return 0;
 //        return ((AirportJoinKey) a).getAirportId() - ((AirportJoinKey) b).getAirportId();
     }
