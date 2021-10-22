cf9c535d420fd12dfe862c3f058512b83fa71978
ZhukDima <zhuk@gmail.com>
2021-10-23 02:44:04 +0300

    Scripted auto-commit on change (2021-10-23 02:44:04) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -13,7 +13,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportJoinKey, Tex
         }
 
         Flight flight = Flight.parseCSV(value.toString());
-        if (flight.getCode() > 0) {
+        if (flight.getDelay() > 0) {
             context.write(
                     new AirportJoinKey(flight.getCode(), false),
                     new Text(String.valueOf(flight.getDelay()))
