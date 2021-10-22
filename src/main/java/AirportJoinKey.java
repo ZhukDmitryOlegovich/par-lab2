4edaa14e47501c4378c44736c6f582c4f0236c59
ZhukDima <zhuk@gmail.com>
2021-10-22 22:53:21 +0300

    Scripted auto-commit on change (2021-10-22 22:53:21) by gitwatch.sh

--- src/main/java/AirportJoinKey.java
+++ src/main/java/AirportJoinKey.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class AirportJoinKey {
+import org.apache.hadoop.io.WritableComparable;
+
+public class AirportJoinKey implements WritableComparable<AirportJoinKey> {
+    private int airportId;
+    private byte isAirport;
+
+    AirportJoinKey(int airportId, byte isAirport) {
+        this.airportId = airportId;
+        this.isAirport = isAirport;
+    }
+
+    public int getAirportId() {
+        return airportId;
+    }
+
+    @Override
+    public int compareTo(AirportJoinKey o) {
+        return airportId - o.airportId || isAirport - o.isAirport;
+    }
 }
