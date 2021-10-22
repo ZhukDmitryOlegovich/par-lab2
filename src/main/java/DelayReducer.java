464c3f018e928a3b97b306483efbf52d15dc297d
ZhukDima <zhuk@gmail.com>
2021-10-22 23:59:17 +0300

    Scripted auto-commit on change (2021-10-22 23:59:17) by gitwatch.sh

--- src/main/java/DelayReducer.java
+++ src/main/java/DelayReducer.java
@@ -8,5 +8,7 @@ public class DelayReducer extends Reducer<AirportJoinKey, Text, Text, Text> {
     @Override
     protected void reduce(AirportJoinKey key, Iterable<Text> values, Reducer<AirportJoinKey, Text, Text, Text>.Context context) throws IOException, InterruptedException {
         Iterator<Text> it = values.iterator();
+
+        it.forEachRemaining();
     }
 }
